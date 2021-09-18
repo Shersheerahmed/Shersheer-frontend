55d52549702c75acc16a2f753592b5eb775fcc19
Shersheerahmed <89246687+Shersheerahmed@users.noreply.github.com>
2021-09-18 16:13:05 +0300

    Update scores.js

--- js/scores.js
+++ js/scores.js
@@ -2,5 +2,14 @@
 //which fetch all names and scores from rhe database
 
 getScores().then((data)=>{
-    console.log(data)
+    //console.log(data)
+    let display = ''
+    for(let i=0;i<data.length; i++){
+        display += `<li class="score-item header">
+        <span>${data[i].name}</span>   
+        <span>${data[i].score}</span>
+    </li>`
+    }
+    document.getElementById("displayScores").innerHTML = display
 })
+
